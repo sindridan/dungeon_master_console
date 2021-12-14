76e668621a6a39ed3c4588ea784bea299b008d14
Sindri Dan Garðarsson <sindridan@gmail.com>
2021-12-14 14:46:21 +0000

    dice calc files added

---
+++ main/mechanics/dice_calculator/dice_calculator.cpp
@@ -0,0 +1,8 @@
+//
+//  dice_calculator.cpp
+//  dungeon_master_console
+//
+//  Created by Sindri Dan Garðarsson on 14.12.2021.
+//
+
+#include "dice_calculator.hpp"
---
+++ main/mechanics/dice_calculator/dice_calculator.hpp
@@ -0,0 +1,13 @@
+//
+//  dice_calculator.hpp
+//  dungeon_master_console
+//
+//  Created by Sindri Dan Garðarsson on 14.12.2021.
+//
+
+#ifndef dice_calculator_hpp
+#define dice_calculator_hpp
+
+#include <stdio.h>
+
+#endif /* dice_calculator_hpp */
