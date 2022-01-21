f967367d38edad12664623fca45cf92738f5d875
Sindri Dan Garðarsson <sindridan@gmail.com>
2022-01-21 18:54:21 +0000

    character sheet updated

--- main/data_layer/playable_character/characer_sheet.hpp
+++ main/data_layer/playable_character/characer_sheet.hpp
@@ -15,17 +15,22 @@
 #include <string.h>
 
 using namespace std;
+// TODO: this should be a parent class which smaller classes inherit
+// The main purpose of this class is to be the header of the character sheet, it should include child classes
+// so that code can be more reuseable, playable characters, NPCs and monsters alike might share same classes
+
+// TODO: dndClass, dndRace and alignment might need to be classes on their own
 class Character {
     public:
-        Character(); // Dice
-        Character(string name);
-        void set_name(string name);
+        Character();
+        Character(string characterName,string dndClass, int level, string background, string dndRace, string alignment, int experiencePoints, string playerName);
+        void set_name(string playerName);
     
         string get_name();
         friend ostream& operator << (ostream& out, const Character& Character); // overload cout to print the form
 
     private:
-        string name;
+        string playerName;
     
 };
 
